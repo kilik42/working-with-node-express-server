7e1d73e2050e59f5ca7bbf55f36ee697b1f322b5
marvin evins <maevins@cps.edu>
2019-07-13 14:06:08 -0500

    working with express server

---
+++ app.js
@@ -0,0 +1,33 @@
+// load our app server using express somehow...
+
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+  console.log("responding to root route")
+  res.send("hello from ROOOOOT")
+
+})
+
+
+app.get("/users", (req, res) => {
+
+  const user1 = {firstName: "stephen",lastName: "curry"}
+  const user2 = {firstName: "stephen",lastName: "curry"}
+  res.json([user1, user2])
+
+
+  res.send("nodemon auto updates when i save this file")
+})
+
+
+
+
+
+
+
+
+//localhost:3003
+app.listen(3003, ()=> {
+  console.log("your server is up and listening on  3003...")
+})
